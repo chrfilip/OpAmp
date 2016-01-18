8addcb4199d7ee70d950d01f2bad2fc760900b05
Christophoros Filippou <chrfilip@outlook.com>
2016-01-18 15:07:06 +0200

    OpAmp
    Implementation of an Operational Amplifier in Verilog-A

---
+++ opamp_prj/project.ads
@@ -0,0 +1,10 @@
+<ADSProject Revision="1" Version="100">
+    <Project Name="">
+        <Ael Name="save_project_state.ael" />
+        <Preferences Name="layout.prf" />
+        <Preferences Name="mil_layout.prf" />
+        <Preferences Name="mil_schematic.prf" />
+        <Preferences Name="schematic.prf" />
+        <Design Name="opamp.dsn" />
+    </Project>
+</ADSProject>
